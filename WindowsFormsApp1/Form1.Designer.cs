6f9658cd4a4400878a89229adbc39bd6027bb535
Artem <artemvesnin1@gmail.com>
2017-09-26 23:08:17 +0700

    работает 4 кнопки (выход, сброс всех, снять показания, проверка) без разбиения данных на текст бокс

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,7 +29,6 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
-			this.label1 = new System.Windows.Forms.Label();
 			this.button1 = new System.Windows.Forms.Button();
 			this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
 			this.button2 = new System.Windows.Forms.Button();
@@ -48,21 +47,12 @@
 			this.label5 = new System.Windows.Forms.Label();
 			this.label6 = new System.Windows.Forms.Label();
 			this.label7 = new System.Windows.Forms.Label();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
 			this.SuspendLayout();
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(199, 374);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(94, 13);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "нет подключения";
-			this.label1.Click += new System.EventHandler(this.label1_Click_1);
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(38, 362);
+			this.button1.Location = new System.Drawing.Point(38, 350);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(132, 37);
 			this.button1.TabIndex = 1;
@@ -98,6 +88,7 @@
 			this.button4.TabIndex = 5;
 			this.button4.Text = "Сброс всех показаний";
 			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
 			// 
 			// button5
 			// 
@@ -116,6 +107,7 @@
 			this.button6.TabIndex = 7;
 			this.button6.Text = "Выход";
 			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
 			// 
 			// textBox1
 			// 
@@ -213,11 +205,20 @@
 			this.label7.TabIndex = 18;
 			this.label7.Text = "Датчик 5";
 			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(38, 393);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(132, 23);
+			this.progressBar1.TabIndex = 19;
+			this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(791, 428);
+			this.Controls.Add(this.progressBar1);
 			this.Controls.Add(this.label7);
 			this.Controls.Add(this.label6);
 			this.Controls.Add(this.label5);
@@ -235,7 +236,6 @@
 			this.Controls.Add(this.button3);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
-			this.Controls.Add(this.label1);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.Form1_Load);
@@ -245,8 +245,6 @@
 		}
 
 		#endregion
-
-		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Button button1;
 		private System.IO.Ports.SerialPort serialPort1;
 		private System.Windows.Forms.Button button2;
@@ -265,6 +263,7 @@
 		private System.Windows.Forms.Label label5;
 		private System.Windows.Forms.Label label6;
 		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.ProgressBar progressBar1;
 	}
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -11,9 +11,10 @@ using System.IO.Ports;
 using System.Timers;
 using System.IO;
 
+
 namespace WindowsFormsApp1
 {
-	public partial class Form1 : Form
+		public partial class Form1 : Form
 	{
 		public Form1()
 		{
@@ -27,14 +28,30 @@ namespace WindowsFormsApp1
 
 		private void button1_Click(object sender, EventArgs e)
 		{
+			System.Threading.Thread.Sleep(1000);
 			
+			int a = 0;
+			int b=1;
 			string[] portnames = SerialPort.GetPortNames();
-		    label1.Text = portnames[1];
-								                                 
-				
-			}
 
-				private void label1_Click_1(object sender, EventArgs e)
+			foreach (string port in portnames)
+			{
+				if (portnames[a] == "COM1")
+				{
+					
+					a++;
+				}
+				else
+				{ if (a == 1)
+				MessageBox.Show("Подключено", port);
+					b++;
+				}
+			}
+			if (b==1)
+			MessageBox.Show("Контроллер не найден", "Ошибка");
+		}
+
+		private void label1_Click_1(object sender, EventArgs e)
 		{
 
 		}
@@ -68,26 +85,82 @@ namespace WindowsFormsApp1
 		{
 			SerialPort STMport;
 			string[] portnames = SerialPort.GetPortNames();
-			
-			STMport = new SerialPort();
-			STMport.PortName = portnames[1];
-			STMport.BaudRate = 9600;
-			STMport.DataBits = 8;
-			STMport.Parity = System.IO.Ports.Parity.None;
-			STMport.StopBits = System.IO.Ports.StopBits.One;
-			STMport.ReadBufferSize = 16;
-			STMport.ReadTimeout = 10000;
-			STMport.WriteTimeout = 10000;
-			STMport.DtrEnable = true;
-			STMport.Open();
 
-			textBox5.Text = STMport.ReadLine();
-			STMport.Close();
+			int a = 0;
+			int b = 1;
+			foreach (string port in portnames)
+			{
+				if (portnames[a] == "COM1")
+				{
+					a++;
+				}
+				else
+				{
+					if (a == 1)
+					b++;
+					STMport = new SerialPort();
+					STMport.PortName = portnames[1];
+					STMport.BaudRate = 9600;
+					STMport.DataBits = 8;
+					STMport.Parity = System.IO.Ports.Parity.None;
+					STMport.StopBits = System.IO.Ports.StopBits.One;
+					STMport.ReadBufferSize = 16;
+					STMport.ReadTimeout = 1000;
+					STMport.DtrEnable = true;
+					STMport.Open();
+					textBox5.Text = STMport.ReadLine();
+					STMport.Close();
+				}
+			}
+			if (b == 1)
+				MessageBox.Show("Проверте подключение", "Ошибка");
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
 			
 		}
+
+		private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void progressBar1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			textBox5.Text = null;
+			textBox4.Text = null;
+			textBox3.Text = null;
+			textBox2.Text = null;
+			textBox1.Text = null;
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+
+			if (MessageBox.Show("Закрыть?", "Подтверждение", MessageBoxButtons.YesNo) != DialogResult.No) this.Close();
+						
+		}
 	}
+	
 }
