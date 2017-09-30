6b69375767c87570fcf4d1fba3cc17b55402d70b
Artem <artemvesnin1@gmail.com>
2017-09-30 12:23:43 +0700

    работают все кнопки создана кнопка настроек и форма2 для нее (только начал) данные в один столбик сохраняются в текстовый документ

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,6 +29,7 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.button1 = new System.Windows.Forms.Button();
 			this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
 			this.button2 = new System.Windows.Forms.Button();
@@ -48,6 +49,10 @@
 			this.textBox6 = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
 			this.button5 = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// button1
@@ -102,6 +107,7 @@
 			// 
 			// textBox1
 			// 
+			this.textBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
 			this.textBox1.Location = new System.Drawing.Point(53, 88);
 			this.textBox1.Multiline = true;
 			this.textBox1.Name = "textBox1";
@@ -111,6 +117,7 @@
 			// 
 			// textBox2
 			// 
+			this.textBox2.Cursor = System.Windows.Forms.Cursors.Arrow;
 			this.textBox2.Location = new System.Drawing.Point(176, 88);
 			this.textBox2.Multiline = true;
 			this.textBox2.Name = "textBox2";
@@ -119,6 +126,7 @@
 			// 
 			// textBox3
 			// 
+			this.textBox3.Cursor = System.Windows.Forms.Cursors.Arrow;
 			this.textBox3.Location = new System.Drawing.Point(296, 88);
 			this.textBox3.Multiline = true;
 			this.textBox3.Name = "textBox3";
@@ -127,7 +135,8 @@
 			// 
 			// textBox4
 			// 
-			this.textBox4.Location = new System.Drawing.Point(414, 88);
+			this.textBox4.Cursor = System.Windows.Forms.Cursors.Arrow;
+			this.textBox4.Location = new System.Drawing.Point(407, 88);
 			this.textBox4.Multiline = true;
 			this.textBox4.Name = "textBox4";
 			this.textBox4.Size = new System.Drawing.Size(89, 232);
@@ -135,6 +144,7 @@
 			// 
 			// textBox5
 			// 
+			this.textBox5.Cursor = System.Windows.Forms.Cursors.Arrow;
 			this.textBox5.Location = new System.Drawing.Point(532, 88);
 			this.textBox5.Multiline = true;
 			this.textBox5.Name = "textBox5";
@@ -190,18 +200,17 @@
 			// 
 			// textBox6
 			// 
-			this.textBox6.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
-			this.textBox6.Location = new System.Drawing.Point(176, 31);
+			this.textBox6.Location = new System.Drawing.Point(417, 35);
 			this.textBox6.Name = "textBox6";
 			this.textBox6.Size = new System.Drawing.Size(212, 20);
 			this.textBox6.TabIndex = 13;
-			this.textBox6.Text = "испытание";
+			this.textBox6.Text = "Испытание";
 			this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(53, 34);
+			this.label1.Location = new System.Drawing.Point(293, 40);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(118, 13);
 			this.label1.TabIndex = 19;
@@ -216,12 +225,46 @@
 			this.button5.TabIndex = 20;
 			this.button5.Text = "Настройки";
 			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(152, 32);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 22;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(50, 38);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(96, 13);
+			this.label2.TabIndex = 23;
+			this.label2.Text = "Мера измерения:";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+			this.pictureBox1.Location = new System.Drawing.Point(788, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(60, 56);
+			this.pictureBox1.TabIndex = 25;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.WaitOnLoad = true;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(860, 428);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.comboBox1);
 			this.Controls.Add(this.button5);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.label7);
@@ -243,6 +286,7 @@
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -268,6 +312,9 @@
 		private System.Windows.Forms.Label label7;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.PictureBox pictureBox1;
 	}
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,10 +12,11 @@ using System.Timers;
 using System.IO;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
 
-namespace WindowsFormsApp1
+ namespace WindowsFormsApp1
 {
-		public partial class Form1 : Form
+	public partial class Form1 : Form
 	{
+		static public double mnozh;
 		public string probel = "                    ";
 		public string dete1 = "";
 		public string dete2 = "";
@@ -25,17 +26,27 @@ namespace WindowsFormsApp1
 		public string Nazvanie1 = "Test";
 		public string N = "";
 		public int nom = 1;
-		public int status=1;
+		public int status = 1;
+
 
 		public Form1()
 		{
 			InitializeComponent();
+			List<mera> izmer = new List<mera>
+		{
+		    new mera { Id=2, Name="Граммы", Mn=1},
+			new mera { Id=1, Name="Миллиграммы", Mn=1000},
+			new mera { Id=3, Name="Килограммы", Mn=0.001}
+		};
+			comboBox1.DataSource = izmer;
+			comboBox1.DisplayMember = "Name";
+			comboBox1.ValueMember = "Id";
 		}
 		//  КНОПКА ПОДКЛЮЧЕНИЯ
 		private void button1_Click(object sender, EventArgs e)
 		{
 			int a = 0;
-			int b=0;
+			int b = 0;
 			string[] portnames = SerialPort.GetPortNames();
 
 			foreach (string port in portnames)
@@ -46,7 +57,7 @@ namespace WindowsFormsApp1
 				}
 				else
 				{ if (a == 1)
-				MessageBox.Show("Подключено", port);
+						MessageBox.Show("Подключено", port);
 					b++;
 					status = 0;
 				}
@@ -71,40 +82,45 @@ namespace WindowsFormsApp1
 					N = nom.ToString();
 					Nazvanie1 = D + Nazvanie + N + txt;
 					nom++;
-					MessageBox.Show("Файл " + Nazvanie +N+ txt + " создан", "Файл создан");
+					MessageBox.Show("Файл " + Nazvanie + N + txt + " создан", "Файл создан");
 				}
 				else
 				{ Nazvanie = textBox6.Text;
-					Nazvanie1 = D + Nazvanie  + txt;
-					MessageBox.Show("Файл "+Nazvanie + txt+" создан" , "Файл создан");
+					Nazvanie1 = D + Nazvanie + txt;
+					MessageBox.Show("Файл " + Nazvanie + txt + " создан", "Файл создан");
 				}
 				
 				StreamWriter file = new StreamWriter(Nazvanie1);
-				char razdelitel = ' ';
+				string words1 = textBox1.Text;
+				string words2 = textBox2.Text;
+				string words3 = textBox3.Text;
+				string words4 = textBox4.Text;
+				string words5 = textBox5.Text;
+				/*char razdelitel = ' ';
 				string[] words1 = textBox1.Text.Split(razdelitel);
 				string[] words2 = textBox2.Text.Split(razdelitel);
 				string[] words3 = textBox3.Text.Split(razdelitel);
 				string[] words4 = textBox4.Text.Split(razdelitel);
-				string[] words5 = textBox5.Text.Split(razdelitel);
-
+				string[] words5 = textBox5.Text.Split(razdelitel);*/
+				int coll1=textBox1.Lines.Length-1;
 
 				for (int i = 0; i < words1.Length; i++)
 				{
 
-					file.WriteLine((words1[i]) + ("  " + words2[i]) + ("  " + words3[i]) + ("  " + words4[i]) + ("  " + words5[i]));
+					file.WriteLine(words1 +  words2 + words3 + words4 + words5);
 				}
 
 
 				file.Close();
 			}
 		}
-		
+
 		//     ВЫВОД ДАННЫХ
 		private void button2_Click(object sender, EventArgs e)
 		{
 			SerialPort STMport;
 			string[] portnames = SerialPort.GetPortNames();
-			string resylt=" ";
+			string resylt = " ";
 			string data1 = " ";
 			string data2 = " ";
 			string data3 = " ";
@@ -124,9 +140,9 @@ namespace WindowsFormsApp1
 					a++;
 				}
 				else
-				{  
+				{
 					if (a == 1)
-					b++;
+						b++;
 					STMport = new SerialPort();
 					STMport.PortName = portnames[1];
 					STMport.BaudRate = 9600;
@@ -158,34 +174,37 @@ namespace WindowsFormsApp1
 					data4 = (words[3]);
 					data5 = (words[4]);
 				}
-				Data1 = double.Parse(data1);     //для работы с показаниями 
-				Data2 = double.Parse(data2);
-				Data3 = double.Parse(data3);
-				Data4 = double.Parse(data4);
-				Data5 = double.Parse(data5);      
-				dete1 = data1 + probel + dete1;
-				dete2 = data2 + probel + dete2;
-				dete3 = data3 + probel + dete3;
-				dete4 = data4 + probel + dete4;
-				dete5 = data5 + probel + dete5;
+				Data1 = double.Parse(data1)* mnozh;     //для работы с показаниями 
+				Data2 = double.Parse(data2)* mnozh;
+				Data3 = double.Parse(data3)* mnozh;
+				Data4 = double.Parse(data4)* mnozh;
+				Data5 = double.Parse(data5)* mnozh;
+				dete1 = Data1.ToString() + Environment.NewLine + dete1;
+				dete2 = Data2.ToString() + Environment.NewLine + dete2;
+				dete3 = Data3.ToString() + Environment.NewLine + dete3;
+				dete4 = Data4.ToString() + Environment.NewLine + dete4;
+				dete5 = Data5.ToString() + Environment.NewLine + dete5; 
+
 				textBox1.Text = (dete1);
 				textBox2.Text = (dete2);
 				textBox3.Text = (dete3);
 				textBox4.Text = (dete4);
 				textBox5.Text = (dete5);
+
+				
 			}
 		}
 
-		 //   СБРОС ПОКАЗАНИЙ
+		//   СБРОС ПОКАЗАНИЙ
 		private void button4_Click(object sender, EventArgs e)
 		{
 			full_delete();
 		}
 
-		//   ВЫХОД
+		//    ВЫХОД
 		private void button6_Click(object sender, EventArgs e)
 		{
-			if (MessageBox.Show("Выйти?", "Подтверждение", MessageBoxButtons.YesNo) != DialogResult.No) this.Close();		
+			if (MessageBox.Show("Выйти?", "Подтверждение", MessageBoxButtons.YesNo) != DialogResult.No) this.Close();
 		}
 
 		public void full_delete()
@@ -202,35 +221,56 @@ namespace WindowsFormsApp1
 			dete5 = "";
 		}
 
+		//    НАСТРОЙКИ
+		public void button5_Click(object sender, EventArgs e)
+		{
+			new Form3().Show();
+		}
+
+		//    МЕРА ИЗМЕРЕНИЯ
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			mera izmer = (mera)comboBox1.SelectedItem;
+			mnozh = izmer.Mn;
+		}
+		public class mera
+		{
+			public int Id { get; set; }
+			public string Name { get; set; }
+			public double Mn { get; set; }
+		}
 		//Ниже находится не задействованный хлам)
 		private void Form1_Load(object sender, EventArgs e)
-		{		}
+		{ }
 		private void textBox6_TextChanged(object sender, EventArgs e)
-		{		}
+		{ }
 		private void textBox7_TextChanged(object sender, EventArgs e)
-		{		}
+		{ }
 		private void label1_Click_2(object sender, EventArgs e)
-		{		}
+		{ }
 		private void label1_Click_3(object sender, EventArgs e)
-		{		}
+		{ }
 		private void label1_Click_1(object sender, EventArgs e)
-		{		}
+		{ }
 		private void label2_Click(object sender, EventArgs e)
-		{		}
+		{ }
 		private void label1_Click(object sender, EventArgs e)
-		{		}
+		{ }
 		private void textBox1_TextChanged(object sender, EventArgs e)
-		{		}
+		{ }
 		private void label3_Click(object sender, EventArgs e)
-		{		}
+		{ }
 		private void textBox5_TextChanged(object sender, EventArgs e)
-		{		}
+		{ }
 
 		private void label2_Click_1(object sender, EventArgs e)
 		{
 
 		}
 
+
 	}
 	
 }
+	
+
